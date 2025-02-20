6c6b2d25e8ae65cc70465db947af7c8a2b6d33e3
Saman <saman1726@gmail.com>
2025-02-20 11:19:44 +0100

    update

--- helpWorkspace/backend/src/main/java/com/example/expensetracker/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.expensetracker.repository;
-
-import com.example.expensetracker.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
